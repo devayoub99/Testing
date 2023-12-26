1301e9d189b5d1f994a8fdbf9e3fb5ae3668bfe1
Zaid <zaid.al.ogaili.10@gmail.com>
2023-12-26 09:50:19 +0300

    removed aws stuff and others stuff not needed

--- functions/register.js
+++ functions/register.js
@@ -10,7 +10,53 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/.netlify/functions/register', async (req, res) => {
-  // Your registration route logic here
+  try {
+    const { userType, username, email, password } = req.body;
+
+    // Check if the email is already registered
+    const existingUser = await prisma.customer.findUnique({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Email is already registered.' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user based on userType
+    let newUser;
+    if (userType === 'admin') {
+      newUser = await prisma.admin.create({
+        data: {
+          username,
+          email,
+          password: hashedPassword,
+        },
+      });
+    } else if (userType === 'company') {
+      newUser = await prisma.company.create({
+        data: {
+          name: username,
+          email,
+          password: hashedPassword,
+        },
+      });
+    } else if (userType === 'customer') {
+      newUser = await prisma.customer.create({
+        data: {
+          username,
+          email,
+          password: hashedPassword,
+        },
+      });
+    } else {
+      return res.status(400).json({ error: 'Invalid userType.' });
+    }
+
+    return res.status(201).json({ message: 'Registration successful', user: newUser });
+  } catch (error) {
+    console.error('Error during registration:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 module.exports = app;
