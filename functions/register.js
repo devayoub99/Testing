03d3303ee8ec8c84c7bc6ae1ecb1d9f41028089f
Zaid <zaid.al.ogaili.10@gmail.com>
2023-12-27 19:27:10 +0300

    updated register functins to aff cros policy

--- functions/register.js
+++ functions/register.js
@@ -9,6 +9,17 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(bodyParser.json());
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your frontend domain
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
+
 app.post('/.netlify/functions/register', async (req, res) => {
   try {
     const { userType, username, email, password } = req.body;
