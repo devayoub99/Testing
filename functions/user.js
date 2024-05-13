970c46b1243bad97ab7da8663a6dfd3989bf713f
zaid-alogaili <zaid.al-ogaili@circlekeurope.com>
2024-05-13 20:53:44 +0300

    Trying with netlify -9

--- functions/user.js
+++ functions/user.js
@@ -5,13 +5,35 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const app = express();
 
-app.get('/.netlify/functions/companies', async (req, res) => {
+app.get('/.netlify/functions/user/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  const userType = req.query.userType;
   try {
-    const companies = await prisma.company.findMany();
-    res.status(200).json(companies);
+    let user;
+    if (userType === "customer") {
+      user = await prisma.customer.findUnique({
+        where: { id: userId },
+      });
+    } else if (userType === "company") {
+      user = await prisma.company.findUnique({
+        where: { id: userId },
+      });
+    } else if (userType === "admin") {
+      user = await prisma.admin.findUnique({
+        where: { id: userId },
+      });
+    } else {
+      return res.status(400).json({ error: "Invalid user type" });
+    }
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(user);
   } catch (error) {
-    console.error("Failed to fetch companies:", error);
-    res.status(500).json({ error: "Failed to fetch companies" });
+    console.error("Error retrieving user data:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
