292e97a3d3f0d5abe9342b954699e02a3e945d70
zaid-alogaili <zaid.al-ogaili@circlekeurope.com>
2024-05-13 20:39:04 +0300

    Trying with netlify -7

--- functions/users.js
+++ functions/users.js
@@ -1,4 +1,3 @@
-// functions/fetchUsers.js
 const express = require('express');
 const serverless = require('serverless-http');
 const { PrismaClient } = require('@prisma/client');
@@ -6,15 +5,7 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const app = express();
 
-// CORS middleware
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
-app.get('/.netlify/functions/fetchUsers', async (req, res) => {
+app.get('/.netlify/functions/users', async (req, res) => {
   try {
     const customers = await prisma.customer.findMany();
     const companies = await prisma.company.findMany();
@@ -22,8 +13,8 @@ app.get('/.netlify/functions/fetchUsers', async (req, res) => {
 
     res.status(200).json({ customer: customers, company: companies, admin: admins });
   } catch (error) {
-    console.error('Failed to fetch users:', error);
-    res.status(500).json({ error: 'Failed to fetch users' });
+    console.error("Failed to fetch users:", error);
+    res.status(500).json({ error: "Failed to fetch users" });
   }
 });
 
