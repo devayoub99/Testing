c174f83e5cdf3854f950905a4a73802f3c3b9ddd
zaid-alogaili <zaid.al-ogaili@circlekeurope.com>
2024-05-13 21:50:36 +0300

    Trying with netlify -10

--- functions/login.js
+++ functions/login.js
@@ -1,18 +1,30 @@
+// functions/login.js
 const express = require('express');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
 const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); // Import the jsonwebtoken library
 
 const prisma = new PrismaClient();
 const app = express();
 app.use(bodyParser.json());
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your frontend domain
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
+
 app.post('/.netlify/functions/login', async (req, res) => {
   const { email, password } = req.body;
 
-  console.log("Our Credentials", email, password);
+  console.log('Our Credentials', email, password);
 
   try {
     // Check against customer table
@@ -21,16 +33,18 @@ app.post('/.netlify/functions/login', async (req, res) => {
     if (customer && (await bcrypt.compare(password, customer.password))) {
       // Login successful for customer
       const token = jwt.sign(
-        { userId: customer.id, userType: "customer" },
-        "your-secret-key",
-        { expiresIn: "1h" }
+        { userId: customer.id, userType: 'customer' },
+        'your-secret-key',
+        { expiresIn: '1h' }
       );
-      console.log("Login successful for customer:", customer);
+      console.log('Login successful for customer:', customer);
       return res.status(200).json({
-        message: "Login successful",
-        ...customer,
+        message: 'Login successful',
         userId: customer.id,
         token,
+        username: customer.username,
+        email: customer.email,
+        userType: 'customer',
       });
     }
 
@@ -44,21 +58,23 @@ app.post('/.netlify/functions/login', async (req, res) => {
         (await bcrypt.compare(password, company.password))
       ) {
         const token = jwt.sign(
-          { userId: company.id, userType: "company" },
-          "your-secret-key",
-          { expiresIn: "1h" }
+          { userId: company.id, userType: 'company' },
+          'your-secret-key',
+          { expiresIn: '1h' }
         );
-        console.log("Login successful for company:", company);
+        console.log('Login successful for company:', company);
         return res.status(200).json({
-          message: "Login successful",
-          ...company,
+          message: 'Login successful',
           userId: company.id,
           token,
+          username: company.username,
+          email: company.email,
+          userType: 'company',
         });
       } else {
         return res.status(401).json({
           error:
-            "Your account is being reviewed by the admin. You will be notified if the admin approves your request",
+            'Your account is being reviewed by the admin. You will be notified if the admin approves your request',
         });
       }
     }
@@ -69,45 +85,27 @@ app.post('/.netlify/functions/login', async (req, res) => {
     if (admin && (await bcrypt.compare(password, admin.password))) {
       // Login successful for admin
       const token = jwt.sign(
-        { userId: admin.id, userType: "admin" },
-        "your-secret-key",
-        { expiresIn: "1h" }
+        { userId: admin.id, userType: 'admin' },
+        'your-secret-key',
+        { expiresIn: '1h' }
       );
-
-      console.log("Login successful for admin:", admin);
+      console.log('Login successful for admin:', admin);
       return res.status(200).json({
-        message: "Login successful",
-        ...admin,
+        message: 'Login successful',
         userId: admin.id,
         token,
-      });
-    }
-
-    const superAdmin = await prisma.superAdmin.findUnique({ where: { email } });
-
-    if (superAdmin && (await bcrypt.compare(password, superAdmin.password))) {
-      // Login successful for admin
-      const token = jwt.sign(
-        { userId: superAdmin.id, userType: "superAdmin" },
-        "your-secret-key",
-        { expiresIn: "1h" }
-      );
-
-      console.log("Login successful for superAdmin:", superAdmin);
-      return res.status(200).json({
-        message: "Login successful",
-        ...superAdmin,
-        userId: superAdmin.id,
-        token,
+        username: admin.username,
+        email: admin.email,
+        userType: 'admin',
       });
     }
 
     // User not found or incorrect password
-    console.log("Login failed: Invalid credentials");
-    res.status(401).json({ error: "Invalid credentials" });
+    console.log('Login failed: Invalid credentials');
+    res.status(401).json({ error: 'Invalid credentials' });
   } catch (error) {
-    console.error("Login failed:", error);
-    res.status(500).json({ error: "Login failed" });
+    console.error('Login failed:', error);
+    res.status(500).json({ error: 'Login failed' });
   }
 });
 
